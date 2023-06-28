a23e21c50e4114e8090fbdce89bb67173fbba8e0
retooz <38852303+retooz@users.noreply.github.com>
2023-06-28 21:44:46 +0900

    fifth
    s

--- js/getData.js
+++ js/getData.js
@@ -15,7 +15,7 @@ let showCategory = [];
 let showBrand = [];
 const initialize = () => {
     shoes.forEach(element => {
-        if(element.category == 'Running' && !showDataArr.includes(element.shoeName)) {
+        if (element.category == 'Running' && !showDataArr.includes(element.shoeName)) {
             // showDataArr.append(element);
             showHTML.push(`<div class="productWrapper">
             <div class="imgBox">
@@ -30,21 +30,98 @@ const initialize = () => {
                 </div>
             </div>
         </div>`)
-        showDataArr.push(element.shoeName)
+            showDataArr.push(element.shoeName)
         };
     });
     $('.chunkWrapper').html(showHTML);
 }
 initialize();
 
-$('#filter-accept').on('click', function(){
-    
-    $("input[type=checkbox][name=shoe-category]:checked").each(function(){
-        showCategory.push($(this).val());
+$('#filter-accept').on('click', function () {
+    showHTML = [];
+    showDataArr = [];
+    showBrand = [];
+    showCategory = [];
+    $("input[name=shoe-category]:checked").each(function () {
+        if (!showCategory.includes($(this).val)) {
+            showCategory.push($(this).val());
+        }
     });
-    $("input[name=shoe-brand]:checked").each(function() {
-        showBrand.push($(this).val());
+    $("input[name=shoe-brand]:checked").each(function () {
+        if (!showBrand.includes($(this).val)) {
+            showBrand.push($(this).val());
+        }
     })
     console.log(showBrand, showCategory)
+    if (showBrand && showCategory) {
+        shoes.forEach(element => {
+            if (showBrand.includes(element.brand) && showCategory.includes(element.category) && !showDataArr.includes(element.shoeName)) {
+                showHTML.push(`<div class="productWrapper">
+            <div class="imgBox">
+                <img src="src/${element.ShoeId}.jpg" alt="">
+            </div>
+            <div class="productText">
+                <div class="productName">
+                    ${element.brand}
+                </div>
+                <div class="productExp">
+                    ${element.shoeName}
+                </div>
+            </div>
+        </div>`)
+                showDataArr.push(element.shoeName)
+            }
+        })
+    }
+    if (showBrand.length == 0 && showCategory) {
+        shoes.forEach(element => {
+            if (showCategory.includes(element.category) && !showDataArr.includes(element.shoeName)) {
+                showHTML.push(`<div class="productWrapper">
+            <div class="imgBox">
+                <img src="src/${element.ShoeId}.jpg" alt="">
+            </div>
+            <div class="productText">
+                <div class="productName">
+                    ${element.brand}
+                </div>
+                <div class="productExp">
+                    ${element.shoeName}
+                </div>
+            </div>
+        </div>`)
+                showDataArr.push(element.shoeName)
+            }
+        })
+    }
+    if (showBrand && showCategory.length == 0) {
+        shoes.forEach(element => {
+            if (showBrand.includes(element.brand) && !showDataArr.includes(element.shoeName)) {
+                showHTML.push(`<div class="productWrapper">
+            <div class="imgBox">
+                <img src="src/${element.ShoeId}.jpg" alt="">
+            </div>
+            <div class="productText">
+                <div class="productName">
+                    ${element.brand}
+                </div>
+                <div class="productExp">
+                    ${element.shoeName}
+                </div>
+            </div>
+        </div>`)
+                showDataArr.push(element.shoeName)
+            }
+        })
+    }
+    if (showBrand.length == 0 && showCategory.length == 0) {
+        shoes.forEach(element => {
+            if (!showDataArr.includes(element.shoeName)) {
+                showHTML.push(`<div class="productWrapper"><div class="imgBox"><img src="src/${element.ShoeId}.jpg" alt=""></div><div class="productText"><div class="productName">${element.brand}</div><div class="productExp">${element.shoeName}</div></div></div>`)
+                showDataArr.push(element.shoeName)
+            }
+        })
+    }
+    $('.chunkWrapper').html(showHTML);
+
 })
 
--- js/main.js
+++ js/main.js
@@ -17,13 +17,11 @@ $(document).ready(function() {
     })
 
     $('input[name=shoe-category]').change(function(){
-        $('.selection:nth-child(2)').fadeIn(500);
+        $('.selection:nth-child(6)').fadeIn(500);
     })
     $('input[name=shoe-brand]').change(function(){
-        $('.selection:nth-child(3)').fadeIn(500);
+        $('.selection:nth-child(7)').fadeIn(500);
     })
-    $('input[name=shoe-color]').change(function(){
-        $('.selection:nth-child(4)').fadeIn(500);
-    })
+
     
 })
