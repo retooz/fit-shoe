9372eda3aaef2300a7b0f6dc78203f94258158d1
retooz <38852303+retooz@users.noreply.github.com>
2023-06-28 15:39:50 +0900

    second
    second

--- js/main.js
+++ js/main.js
@@ -1,33 +1,31 @@
-
 $(document).ready(function() {
     $('section').css('height', ($('section').css('height')))
     $('.selection').hide();
-    $('.selection:first').show();
+    $('.selection:first').fadeIn(500);
 
     $('input[name=feet-width]').change(function(){
-        $('.selection:nth-child(2)').show();
+        $('.selection:nth-child(2)').fadeIn(500);
     })
     $('input[name=feet-height]').change(function(){
-        $('.selection:nth-child(3)').show();
+        $('.selection:nth-child(3)').fadeIn(500);
     })
     $('input[name=feet-flat]').change(function(){
-        $('.selection:nth-child(4)').show();
+        $('.selection:nth-child(4)').fadeIn(500);
     })
     $('input[name=shoe-gender]').change(function(){
-        $('.selection:nth-child(5)').show();
+        $('.selection:nth-child(5)').fadeIn(500);
     })
 
     $('input[name=shoe-category]').change(function(){
-        $('.selection:nth-child(2)').show();
+        $('.selection:nth-child(2)').fadeIn(500);
     })
     $('input[name=shoe-brand]').change(function(){
-        $('.selection:nth-child(3)').show();
+        $('.selection:nth-child(3)').fadeIn(500);
     })
     $('input[name=shoe-price]').change(function(){
-        $('.selection:nth-child(4)').show();
+        $('.selection:nth-child(4)').fadeIn(500);
     })
     $('input[name=shoe-color]').change(function(){
-        $('.selection:nth-child(5)').show();
+        $('.selection:nth-child(5)').fadeIn(500);
     })
-    
 })
